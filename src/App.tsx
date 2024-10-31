2aaa40879a30ee9292d52612b9cbb08cfa54ef73
yunshin <2younsin@gmail.com>
2024-10-31 23:41:37 +0900

    refactor: navigation 출력

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Outlet, Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
+import styled from "styled-components";
+import Navigation from "./components/Navigation";
 import Favorite from "./pages/Favorite";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Navigation from "./components/Navigation";
 import NotFound from "./pages/NotFound";
-import styled from "styled-components";
 
 const AppContainer = styled.div`
   position: relative;
@@ -23,20 +23,24 @@ const Content = styled.div`
 `;
 
 function App() {
-  const isLogin = location.pathname === "/";
-
   return (
     <Router>
       <AppContainer>
         <Content>
           <Routes>
             <Route path="/" element={<Login />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/favorite" element={<Favorite />} />
+            <Route element={ 
+              <>
+                <Outlet />
+                <Navigation/>
+              </>
+            }>
+              <Route path="/home" element={<Home />} />
+              <Route path="/favorite" element={<Favorite />} />
+            </Route>
             <Route path="*" element={<NotFound />} />
           </Routes>
         </Content>
-        {isLogin ? null : <Navigation />}
       </AppContainer>
     </Router>
   );
