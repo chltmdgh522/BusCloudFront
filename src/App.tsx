3d804106eaa19cd8ba56cad17243684c0680ea29
SeokHun <tommya98@naver.com>
2024-10-31 19:13:26 +0900

    feat: 네비게이션 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 import Favorite from "./pages/Favorite";
-import Header from "./components/Header";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Navigation from "./components/Navigation";
 import NotFound from "./pages/NotFound";
 import styled from "styled-components";
 
 const AppContainer = styled.div`
-  width: 500px;
+  position: relative;
+  width: 390px;
   margin: 0 auto;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
   background-color: white;
@@ -22,19 +23,20 @@ const Content = styled.div`
 `;
 
 function App() {
+  const isLogin = location.pathname === "/";
+
   return (
     <Router>
       <AppContainer>
-        <Header />
-
         <Content>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
             <Route path="/favorite" element={<Favorite />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </Content>
+        {isLogin ? null : <Navigation />}
       </AppContainer>
     </Router>
   );
